25e8c0df7435a6c6e71c8b937e60ac77622019b6
Margui <margui.berjon@gmail.com>
2020-08-04 11:48:31 +0200

    Feature: error message

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 import * as UI from './interfaz.js';
-console.log(UI);
+
+UI.formularioBuscar.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const artista = document.querySelector('#artista').value,
+    cancion = document.querySelector('#cancion').value;
+
+  if (artista === '' || cancion === '') {
+    UI.divMensajes.innerHTML = 'Todos los campos son obligatorios';
+    UI.divMensajes.classList.add('error');
+  }
+
+
+});
+
+
